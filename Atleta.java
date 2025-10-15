e50390cd16b5d483c7a0204b84733220daeb88de
Vitor hugo <99295735+VitorGoulartt@users.noreply.github.com>
2025-10-15 00:04:51 +0000

    adicionei a classe atleta estou a fazer a atividade 3

--- Atleta.java
+++ Atleta.java
@@ -4,5 +4,48 @@ public class Atleta {
     private int idade;
     private double pesokg;
     private double alturaMetros;
-    
+
+    public Atleta(String nome, String modalidade, int idade, double pesokg, double alturaMetros){
+        this.nome = nome;
+        this.modalidade = modalidade;
+        this.idade = idade;
+        this.pesokg = pesokg;
+        this.alturaMetros = alturaMetros;
+    }
+    public String getnome(){
+        return nome;
+    }
+    public String getmodalidade(){
+        return modalidade;
+    }
+    public int getidade(){
+        return idade;
+    }
+    public double getpesokg(){
+        return pesokg;
+    }
+    public double getalturaMetros(){
+        return alturaMetros;
+    }
+    public void setnome(String nome){
+        this.nome = nome;
+    }
+    public void setmodalidade(String modalidade){
+        this.modalidade = modalidade;
+    }
+    public void setidade(int idade){
+        this.idade = idade;
+    }
+    public void setpesokg(double pesokg){
+        this.pesokg = pesokg;
+    }
+    public void setalturaMetros(double alturaMetros){
+        this.alturaMetros = alturaMetros;
+    }
+    public void calcularIMC(){
+
+        
+    }
+
+
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args){
+        Atleta atleta1 = new Atleta("vitor", "volei", 17, 73, 172 );
         System.out.println("Ola mundo");
         System.out.println("Ola mundo");
+        atleta1.calcularIMC();
+
     }
 }
